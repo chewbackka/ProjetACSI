b2e08e17f3ce669d78da8924b0876d1f01d447f5
chewbackka <leceddu38@gmail.com>
2016-01-05 10:18:01 +0100

    base

---
+++ ProjetsEPSI/Raclette4Every1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Raclette4Every1.MvcApplication" Language="C#" %>
---
+++ ProjetsEPSI/Raclette4Every1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Raclette4Every1.Startup))]
+namespace Raclette4Every1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
