845160d54bf3cf85f502f34047b0ab70bc4e6be5
chewbackka <leceddu38@gmail.com>
2016-01-19 09:38:26 +0100

    ON REFAIT TOUT

---
+++ ProjetsEPSI/RacletteProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RacletteProject.Startup))]
+namespace RacletteProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
